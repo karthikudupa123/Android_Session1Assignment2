66f021415aa4a6388f0475570a52d5b5f76f4729
karthikudupa123 <karthikudupa123@gmail.com>
2016-12-19 11:01:45 +0530

    commit

---
+++ Assgn1_2.java
@@ -0,0 +1,15 @@
+public class Assgn1_2
+{
+ public static void main(String [] args)
+ {
+  int a=20,b=10;
+  b=a-- - --a;
+  int c=a--;
+  int d=a>>2;
+  int e=a&b;
+  System.out.println(b);
+  System.out.println(c);
+  System.out.println(d);
+  System.out.println(e);
+  }
+}
